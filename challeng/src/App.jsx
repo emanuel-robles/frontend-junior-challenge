11adbe3df9d49a9d48463bc324e5936b06f69b1c
emanuel-robles <109557070+emanuel-robles@users.noreply.github.com>
2023-03-16 21:45:32 -0300

    build

--- challeng/src/App.jsx
+++ challeng/src/App.jsx
@@ -6,6 +6,7 @@ import React,{useEffect} from "react";
 import "./App.css";
 import TodoForm from "components/TodoForm";
 import { ToastContainer } from "react-toastify";
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"
 import {fetchAllTasks} from '../src/features/counter/pruebaSlider';
 
 const App = () => {
@@ -17,10 +18,27 @@ useEffect(() => {
 
   return (
     <div className="root">
+      <Router>
+        <div>
+          <Link to='/' >Home</Link>
+        </div>
+        <Switch>
+          <Route path='/'>
+            
       <TodoList />
       <TodoResults />
       <TodoForm/>
       <ToastContainer autoClose={2000} />
+
+
+          </Route>
+        </Switch>
+      </Router>
+      
+      
+      
+      
+      
     </div>
   );
 };
